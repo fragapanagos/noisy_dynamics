28a5fd59b3b9738fff68e18b712aecb305dc2c73
samfok <sam.b.fok@gmail.com>
2013-07-29 23:19:24 -0700

    Separate input from state

--- noisy_system.py
+++ noisy_system.py
@@ -4,35 +4,49 @@ from pycolorplot.colorgen import ColorGenerator as CG
 from distributions import *
 import pdb
 
-def dxdt_cubic(x):
-    return 8./3 * (-x**3 + x) 
+def dxdt_cubic(x, u):
+    return 8./3 * (-(x+u)**3 + (x+u)) 
 
-def dxdt_quad_clip(x):
-    base = x**2 - 1
-    return np.where(np.logical_and(base>0,x>0), 1-x, base)
+def dxdt_quad_clip(x, u):
+    base = (x+u)**2 - 1
+    return np.where(np.logical_and(base>0,(x+u)>0), 1-(x+u), base)
+
+def dxdt_int_with_error(x, u):
+    return 0.1*np.sin(4*np.pi*x) + u
+
+def dxdt_int(x,u):
+    return np.zeros(x.shape) + u
 
 def local_avg(x, dxdt, dist, dxmin=1, dxmax=1, npts=50):
     """Convolves dxdt with dist around x.
 
     Uses the region [x-dxmin: x+dxmax]"""
     if dist.sigma == 0:
-        return dxdt(x)
+        return dxdt(x, 0)
     pts, step = np.linspace(x-dxmin, x+dxmax, num=npts, retstep=True)
-    return np.sum(dxdt(pts) * dist.pdf(pts-x)) * step
+    return np.sum(dxdt(pts, 0) * dist.pdf(pts-x)) * step
+
+def find_stable_pts(x, fx):
+    """finds the stable points of a dynamical system"""
+    passed_zero = np.where(np.diff(np.sign(fx))<0)[0]
+    stable_x = (x[passed_zero+1] + x[passed_zero]) / 2.
+    return stable_x
 
 # simulation parameters
 T = 4       # total simulation time
 dt = 0.001  # simulation timestep
 x0s = np.linspace(-1.5,1.5,10)  # initial conditions
-x = np.linspace(-1.5,1.5,50)    # space to examine phase
+x = np.linspace(-1.5,1.5,100)    # space to examine phase
 
 # select dynamical system
-# dxdt = dxdt_cubic # cubic system
-dxdt = dxdt_quad_clip # quadratic system
+# dxdt = dxdt_cubic 
+dxdt = dxdt_quad_clip 
+# dxdt = dxdt_int_with_error 
+# dxdt = dxdt_int 
 
 # noise parameters 
 max_sigma = 1.              # max noise level
-sigmas = np.linspace(0,2,9) # noise levels
+sigmas = np.linspace(0,max_sigma,9) # noise levels
 noise_sources = [gaussian(mu=0, sigma=sigma) for sigma in sigmas] # gaussian
 # noise_sources = [uniform(mu=0, sigma=sigma) for sigma in sigmas] # uniform
 
@@ -41,11 +55,14 @@ cgen = CG('red') # color generator for plotting
 print 'plotting phase portrait...'
 plt.figure('phase')
 ax = plt.subplot(111)
-ax.plot(x, dxdt(x), color='k')
+ax.plot(x, dxdt(x,0), color='k')
 ax.set_color_cycle(cgen.get_color_list(10))
 for x0 in x0s:
-    plt.plot(x0, dxdt(x0), 'o', markersize=10)
+    plt.plot(x0, dxdt(x0,0), 'o', markersize=10)
 plt.axhline(color='b')
+s_pts = find_stable_pts(x, dxdt(x,0))
+for pt in s_pts:
+    plt.axvline(pt)
 plt.xlabel(r'$x$', fontsize=18)
 plt.ylabel(r'$\dot{x}$', fontsize=18)
 plt.savefig('phase')
@@ -55,12 +72,15 @@ plt.figure('phase_noise')
 for noise_idx, noise_src in enumerate(noise_sources):
     phase_conv = np.zeros(x.shape)
     for i, pt in enumerate(x):
-        phase_conv[i] = local_avg(pt, dxdt, noise_src, dxmin=3, dxmax=3)
+        phase_conv[i] = local_avg(pt, dxdt, noise_src, dxmin=10, dxmax=10, npts=100)
     plt.subplot(3,3,noise_idx+1)
     plt.plot(x, phase_conv, color='k')
     plt.axhline(color='b')
     locs, labels = plt.xticks()
     plt.xticks(locs, locs, rotation=10)
+    s_pts = find_stable_pts(x, phase_conv)
+    for pt in s_pts:
+        plt.axvline(pt)
     # zc_idx = np.where(np.diff(np.sign(phase_conv)))[0][-1]
     # plt.axvline((x[zc_idx]+x[zc_idx+1])/2, color='r')
     plt.title(r'$\sigma=%1.2f$'%sigmas[noise_idx])
@@ -85,7 +105,8 @@ for noise_idx, noise_src in enumerate(noise_sources):
         state = x0
         while now < T:
             now = now + dt
-            state = state + dxdt(state + noise_src.get_sample()) * dt 
+            state = state + dxdt(state, noise_src.get_sample()) * dt # multiplicative noise
+            # state = state + dxdt(state, 0) * dt + noise_src.get_sample() * np.sqrt(dt) # additive noise
             time.append(now)
             states.append(state)
         ax.plot(time, states)
